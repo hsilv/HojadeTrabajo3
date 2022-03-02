8ded8413acdabcb5e83624d75ac38518b8d1aad1
eliasalvarado <77988653+eliasalvarado@users.noreply.github.com>
2022-03-02 14:03:11 -0600

    Creación de Interfaz

---
+++ src/main/Comparador.java
@@ -0,0 +1,14 @@
+/*
+ * Interfaz para realizar las comparaciones entre los enteros que contenga la lista
+ */
+
+ package main;
+
+/**
+ *
+ * @author Elias
+ */
+public interface Comparador
+{
+    public int Compare(int object1, int object2);
+}
