6dbbc13026a1cb5af07a612eb9118fbf8359349b
Sebastián Silva <77755597+hsilv@users.noreply.github.com>
2022-03-04 13:07:43 -0600

    Implementación de manejo de archivos

--- src/main/Sort.java
+++ src/main/Sort.java
@@ -1,5 +1,19 @@
 package main;
+import java.util.*;
 
 public class Sort<T> implements Comparador {
+
+    public int Compare(int object1, int object2){
+        if(object1 > object2){
+            return object1;
+        } else if (object1 < object2){
+            return object2;
+        } else{
+            return object1;
+        }
+    }
     
+    public ArrayList<Integer> bubbleSort(ArrayList<Integer> lista){
+       return null; 
+    }
 }
