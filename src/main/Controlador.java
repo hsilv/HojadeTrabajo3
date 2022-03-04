09a6d190e2d79a084789935524900b640a7a144a
eliasalvarado <77988653+eliasalvarado@users.noreply.github.com>
2022-03-04 09:32:53 -0600

    Update Controlador.java

--- src/main/Controlador.java
+++ src/main/Controlador.java
@@ -4,18 +4,31 @@
  * and open the template in the editor.
  */
 package main;
+import java.util.ArrayList;
+
+import Generador.*;
 
 /**
  *
  * @author Sebastián
  */
 public class Controlador {
-
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        Sort sort = new Sort();
+        Archivo archivo = new Archivo();
+        archivo.crearArchivo("C:\\Users\\ealva \\Documents\\GitHub\\HojadeTrabajo3\\src\\main");
+        archivo.agregarNumeros(100);
+
+        ArrayList<Integer> lista = new ArrayList<>();
+        lista = archivo.leerArchivo();
+        
         System.out.println("Welcome");
+        
+
     }
     
 }
