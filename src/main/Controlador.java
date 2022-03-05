beea78b28438a0ab4a0c498e6aa653cf6f5ba05a
Sebastián Silva <77755597+hsilv@users.noreply.github.com>
2022-03-04 22:04:47 -0600

    Se añadió un menú

--- src/main/Controlador.java
+++ src/main/Controlador.java
@@ -8,6 +8,7 @@ package main;
 import fileGenerator.Principal;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  *
@@ -20,42 +21,45 @@ public class Controlador {
      */
     public static void main(String[] args) {
         Principal p = new Principal();
-        p.generarArchivo("F:\\Java POO\\Programacion\\HojadeTrabajo3\\src\\main\\numeros.txt", 10000);
-        Sort<Integer> sort = new Sort<Integer>();
+        p.generarArchivo("F:\\Java POO\\Programacion\\HojadeTrabajo3\\src\\main\\numeros.txt", 50);
         ArrayList<Integer> numeros = p.leerArchivo();
         ArrayList<Integer> numeros2 = p.leerArchivo();
-        numeros = sort.bubbleSort(numeros);
-        for(Integer numero: numeros){
-            System.out.println(numero);
-        }
-        
-        ArrayList<Integer> lista = new ArrayList<>();
-        lista = p.leerArchivo();
-        int[] nuevaLista = convertir(lista);
+        ArrayList<Integer> numeros3 = p.leerArchivo();
+        Sort<Integer> sort = new Sort<Integer>();
 
-        
-        System.out.println("\nDatos");
-        System.out.println("\n" + lista.toString());
-        
-        int [] ordenados = convertir(sort.gnomeSort(lista));
-        System.out.println("\nOrdenados con Gnome");
-        System.out.println("\n" + Arrays.toString(ordenados));
-        
-        System.out.println("\nOrdenados con Merge");
-        int[] ordenadosMerge = sort.mergeSort(nuevaLista);
-        System.out.println("\n" + Arrays.toString(ordenadosMerge));
+        boolean flag = true;
+        Scanner scan = new Scanner(System.in);
+        while (flag) {
+            System.out.println("Que desea hacer? \n1.BubbleSort \n2.gnomeSort \n3.mergeSort \n4.QuickSort \n5.RadixSort \n6.Salir");
+            int opcion = scan.nextInt();
+            ArrayList<Integer> lista = new ArrayList<>();
+            lista = p.leerArchivo();
+            int[] nuevaLista = convertir(lista);
 
-        System.out.println("\nOrdenados con Quick");
-        ArrayList<Integer> ordenadosQuick = sort.Quicksort(numeros, 100);
-        System.out.println("\n" + ordenadosQuick.toString());
-        
-        numeros2 = sort.Radix(convertir(numeros2), numeros2.size());
-        for(Integer numero: numeros2){
-            System.out.println(numero);
-        }
+            System.out.println("\nDatos");
+            System.out.println("\n" + lista.toString());
+            while (opcion <= 0 || opcion >= 7) {
+                System.out.println("Ingrese una opcion");
+                opcion = scan.nextInt();
+            }
+            if (opcion == 1) {
+
+                numeros = sort.bubbleSort(numeros);
+            } else if (opcion == 2) {
+                int[] ordenados = convertir(sort.gnomeSort(lista));
+            } else if (opcion == 3) {
+                int[] ordenadosMerge = sort.mergeSort(nuevaLista);
+            } else if (opcion == 4) {
+                ArrayList<Integer> ordenadosQuick = sort.Quicksort(numeros, 100);
+            } else if (opcion == 5) {
+                numeros2 = sort.Radix(convertir(numeros2), numeros2.size());
+            } else if(opcion == 6){
+                flag = false;
+            }
     }
+}
 
-    public static int[] convertir(ArrayList<Integer> lista) {
+public static int[] convertir(ArrayList<Integer> lista) {
         int[] nuevo = new int[lista.size()];
         for (int i = 0; i < lista.size(); i++) {
             nuevo[i] = lista.get(i);
